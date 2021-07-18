7680d456c27232c23c55ae21f68c6037c0acd1be
rostunho <83015198+rostunho@users.noreply.github.com>
2021-07-18 20:06:20 +0000

    Deploying to gh-pages from @ rostunho/team-six-icecream@1d04cc7c37043ce360642e09d8bf844f09399e03 🚀

--- src.cd58d6fc.js
+++ src.cd58d6fc.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/advantages/icon1x1.png":[["icon1x1.e40153a1.png","gr21"],"gr21"],"./../images/advantages/icon2x1.png":[["icon2x1.348e9fa6.png","MA8P"],"MA8P"],"./../images/advantages/icon3x1.png":[["icon3x1.1c2573ad.png","kvC7"],"kvC7"],"./../images/customer-reviews/quotes-min.png":[["quotes-min.263b50b6.png","GMfR"],"GMfR"],"./../images/customer-reviews/Group8-min.png":[["Group8-min.b7c6aef8.png","z9PP"],"z9PP"],"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-six-icecream/src.31a47451.js.map
+//# sourceMappingURL=/team-six-icecream/src.cd58d6fc.js.map
