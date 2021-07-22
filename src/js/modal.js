e64e4b66111ae715faa2e24442b00ab30b7870f1
Rostyk Podolets <ros.podolets@gmail.com>
2021-07-22 15:31:47 +0300

    update 3.0
    canceled animation

--- src/js/modal.js
+++ src/js/modal.js
@@ -13,34 +13,34 @@
     }
 })();
   
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-shop-open]'),
-      closeModalBtn: document.querySelector('[data-shop-close]'),
-      modal: document.querySelector('[data-shop]'),
-    };
+// (() => {
+//     const refs = {
+//       openModalBtn: document.querySelector('[data-shop-open]'),
+//       closeModalBtn: document.querySelector('[data-shop-close]'),
+//       modal: document.querySelector('[data-shop]'),
+//     };
   
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
+//     refs.openModalBtn.addEventListener('click', toggleModal);
+//     refs.closeModalBtn.addEventListener('click', toggleModal);
   
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
-    }
-})();
+//     function toggleModal() {
+//       refs.modal.classList.toggle('is-hidden');
+//     }
+// })();
 
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-shop-mob-open]'),
-            modal: document.querySelector('[data-shop]'),
-    };
+// (() => {
+//     const refs = {
+//       openModalBtn: document.querySelector('[data-shop-mob-open]'),
+//             modal: document.querySelector('[data-shop]'),
+//     };
   
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
+//     refs.openModalBtn.addEventListener('click', toggleModal);
+//     refs.closeModalBtn.addEventListener('click', toggleModal);
   
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
-    }
-})();
+//     function toggleModal() {
+//       refs.modal.classList.toggle('is-hidden');
+//     }
+// })();
 
 
 
